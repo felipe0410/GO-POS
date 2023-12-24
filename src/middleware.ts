251181416608bc76b8b8c2c8531c2c4a54c7ff03
felipe0410 <felipe.montana.whitesoft@gmail.com>
2023-12-24 09:52:24 -0500

    correction inventary

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,34 +2,21 @@ import { NextResponse } from 'next/server';
 
 import type { NextRequest } from 'next/server';
 export function middleware(request: NextRequest) {
-    const excludedPaths = ['/_next', '/static', '/sign_in', '/sign_up', '/font'];
+    const excludedPaths = ['/_next', '/static', '/sign_in', '/sign_up', '/font', '.'];
     const getCookie = request.cookies.get('user')?.value;
     const userCookie = getCookie?.length;
-    console.log('userCookie;;;>', userCookie)
-    console.log('llegue aqui1::>', request.url)
-    if (['/sign_in', '/sign_up'].some(path => request.url.includes(path)) && userCookie) {
+    const pathname = request.nextUrl.pathname
+    console.log('llegue aqui1::>', request.nextUrl.pathname)
+    if (['/sign_in', '/sign_up'].some(path => pathname.includes(path)) && userCookie) {
         return NextResponse.redirect(new URL('/', request.url));
     }
-    console.log('llegue aqui2::>', request.url)
-    console.log('test::>', excludedPaths.some(path => request.url.includes(path)))
-    if (excludedPaths.some(path => request.url.includes(path))) {
+    if (excludedPaths.some(path => pathname.includes(path))) {
         return NextResponse.next();
     }
-    console.log('llegue aqui3::>', request.url)
     if (userCookie) {
         return NextResponse.next();
     }
-    console.log('llegue aqui4::>', request.url)
-    return new Response(
-        '<script>window.location.href="/sign_in";</script>',
-        {
-            status: 200,
-            headers: {
-                'Content-Type': 'text/html',
-            },
-        }
-    );
-    // return NextResponse.redirect(new URL('/sign_in', request.url));
+    return NextResponse.redirect(new URL('/sign_in', request.url));
 }
 
 export const config = {
