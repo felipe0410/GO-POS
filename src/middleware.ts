54c451249c74c761e3b82b4f98c1a85d8d62f088
felipe0410 <felipe.montana.whitesoft@gmail.com>
2023-12-26 21:07:40 -0500

    correction functions

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,9 +5,8 @@ export function middleware(request: NextRequest) {
     const excludedPaths = ['/_next', '/static', '/sign_in', '/sign_up', '/font', '.'];
     const allowOrigin = 'https://go-pos-add98.web.app'; // Reemplazar con tu dominio de Firebase Hosting
     const res = NextResponse.next()
-    // add the CORS headers to the response
     res.headers.append('Access-Control-Allow-Origin', '*')
-    res.headers.append('Access-Control-Allow-Origin', allowOrigin)
+    // res.headers.append('Access-Control-Allow-Origin', allowOrigin)
     res.headers.append('Access-Control-Allow-Credentials', 'true')
     const getCookie = request?.cookies?.get('user')?.value ?? "";
     const allCookies = request.cookies.getAll()
@@ -27,15 +26,6 @@ export function middleware(request: NextRequest) {
     if (userCookie > 0) {
         return NextResponse.next();
     }
-    // return new Response(
-    //     '<script>window.location.href="/sign_in";</script>',
-    //     {
-    //         status: 200,
-    //         headers: {
-    //             'Content-Type': 'text/html',
-    //         },
-    //     }
-    // );
     return NextResponse.redirect(new URL('/sign_in', request.url));
 }
 
