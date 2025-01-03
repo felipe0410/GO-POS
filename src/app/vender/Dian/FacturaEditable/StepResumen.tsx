89f8c94760299e88cf41edad05f2824e4877d148
Felipe Montaña <andres.montana.r4@gmail.com>
2025-01-03 17:52:31 -0500

    add invoices dian

--- src/app/vender/Dian/FacturaEditable/StepResumen.tsx
+++ src/app/vender/Dian/FacturaEditable/StepResumen.tsx
@@ -47,6 +47,24 @@ const InvoicePreview = () => {
     setIsDarkMode((prevMode) => !prevMode);
   };
 
+  const getCurrentDateTime = () => {
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = String(now.getMonth() + 1).padStart(2, "0");
+    const day = String(now.getDate()).padStart(2, "0");
+    const hours = String(now.getHours()).padStart(2, "0");
+    const minutes = String(now.getMinutes()).padStart(2, "0");
+    const seconds = String(now.getSeconds()).padStart(2, "0");
+
+    return {
+      date: `${year}-${month}-${day}`,
+      hour: `${hours}:${minutes}:${seconds}`,
+    };
+  };
+
+  
+  const { date, hour } = getCurrentDateTime();
+
   const handleSendToDian = async () => {
     try {
       setIsSubmitting(true);
@@ -72,6 +90,8 @@ const InvoicePreview = () => {
           qrDian: send.qr.qrDian,
           qrUrl: send.qr.url,
           pdfUrl: send.pdf.url,
+          date, 
+          hour,
         };
 
         await createInvoiceDian(
